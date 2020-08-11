f3373ddb7c89efc241967ec0e3adf1ea07189d0b
Kimaru thagana <macbuntu@192.168.1.3>
2020-08-11 14:59:43 +0300

    add database configuration dict

--- diagnosis_microservice/settings.py
+++ diagnosis_microservice/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'diagnosis_microservice.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+'default': {
+        'ENGINE': os.environ.get('ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
