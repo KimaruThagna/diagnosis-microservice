27f022ff443482ef67e8baa32fdf0aaf51d8318f
Kimaru thagana <macbuntu@192.168.1.3>
2020-08-11 15:05:12 +0300

    nginx configuration and docker build file

--- diagnosis_microservice/settings.py
+++ diagnosis_microservice/settings.py
@@ -22,7 +22,7 @@ load_dotenv(dotenv_path=env_path)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG'))
