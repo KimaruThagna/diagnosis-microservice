8f35d8cb5e4c08bcf1138cb3a3e1797b95c13ec8
Kimaru thagana <macbuntu@localhost.localdomain>
2020-08-12 23:02:36 +0300

    add federated objects

--- diagnosis/resolvers/federation/diagnosis.py
+++ diagnosis/resolvers/federation/diagnosis.py
@@ -1,37 +1,33 @@
 from ariadne.contrib.federation import FederatedObjectType
 
-from patient.models import Patient
+from diagnosis.models import Diagnosis
 
-patient_federated_object = FederatedObjectType("Patient")
-
-
-@patient_federated_object.resolve_reference
-def get_patient_by_uid(representation):
-    return Patient.objects.get(patient_number=representation.get("patient_number"))
+diagnosis_federated_object = FederatedObjectType("Diagnosis")
 
 
-@patient_federated_object.field("uid")
+@diagnosis_federated_object.resolve_reference
+def get_diagnosis_by_uid(representation):
+    return Diagnosis.objects.get(id=representation.get("id"))
+
+
+@diagnosis_federated_object.field("uid")
 def resolve_id(obj, *_):
     return obj.uid
 
 
-@patient_federated_object.field("indexingId")
+@diagnosis_federated_object.field("indexingId")
 def resolve_indexing_id(obj, *_):
     return obj.indexing_id
 
 
-@patient_federated_object.field("first_name")
-def resolve_first_name(obj, *_):
-    return obj.first_name
+@diagnosis_federated_object.field("diagnosis")
+def resolve_diagnosis(obj, *_):
+    return obj.diagnosis
 
-@patient_federated_object.field("last_name")
-def resolve_last_name(obj, *_):
-    return obj.last_name
+@diagnosis_federated_object.field("consulting_doctor")
+def resolve_consulting_doctor(obj, *_):
+    return {"id":obj.consulting_doctor}
 
-@patient_federated_object.field("patient_number")
-def resolve_patient_number(obj, *_):
-    return obj.patient_number
-
-@patient_federated_object.field("county")
-def resolve_county(obj, *_):
-    return obj.county
+@diagnosis_federated_object.field("visiting_patient")
+def resolve_visiting_patient(obj, *_):
+    return {"id":obj.visiting_patient}
