672b8fb584ebe311536ed0438107608e054b3489
Kimaru thagana <macbuntu@localhost.localdomain>
2020-08-12 22:11:11 +0300

    add grqphql files

--- diagnosis/models.py
+++ diagnosis/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
 # Create your models here.
+
 class Diagnosis(models.Model):
     diagnosis = models.TextField(max_length=2000)
     consulting_doctor = ArrayField(JSONField(), size=5)
