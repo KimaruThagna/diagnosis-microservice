20b8f1de01c6497b4ae061e62388d93422850ce9
Kimaru thagana <macbuntu@localhost.localdomain>
2020-08-13 11:38:12 +0300

    add federated objects

--- diagnosis/resolvers/federation/diagnosis.py
+++ diagnosis/resolvers/federation/diagnosis.py
@@ -3,7 +3,8 @@ from ariadne.contrib.federation import FederatedObjectType
 from diagnosis.models import Diagnosis
 
 diagnosis_federated_object = FederatedObjectType("Diagnosis")
-
+patient_federated_object = FederatedObjectType("Patient")
+doctor_federated_object = FederatedObjectType("Doctor")
 
 @diagnosis_federated_object.resolve_reference
 def get_diagnosis_by_uid(representation):
@@ -31,3 +32,11 @@ def resolve_consulting_doctor(obj, *_):
 @diagnosis_federated_object.field("visiting_patient")
 def resolve_visiting_patient(obj, *_):
     return {"id":obj.visiting_patient}
+
+@doctor_federated_object.field("diagnosis")
+def resolve_doctor_diagnosis(obj, *_):
+    return Diagnosis.objects.filter(consulting_doctor__license_number=obj.diagnosis)
+
+@patient_federated_object.field("diagnosis")
+def resolve_patient_diagnosis(obj, *_):
+    return Diagnosis.objects.filter(visiting_patient__patient_number=obj.visiting_patient)
